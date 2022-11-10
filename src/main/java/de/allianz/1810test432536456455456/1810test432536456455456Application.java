4f31a3a35117fe676e6372ffc313add31ffe2ecf
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-11-10 10:04:38 +0100

    Create maven project

---
+++ src/main/java/de/allianz/1810test432536456455456/1810test432536456455456Application.java
@@ -0,0 +1,13 @@
+package de.allianz.1810test432536456455456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 1810test432536456455456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(1810test432536456455456Application.class, args);
+	}
+
+}
